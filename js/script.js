f9f3b40b67f29677c157bc901ed3b40e2cdd04c9
NithurjanA <ayadn1@bfh.ch>
2024-11-08 14:32:32 +0000

    Fertigstellung des Projekts

--- js/script.js
+++ js/script.js
@@ -18,3 +18,27 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = "auto";
       });
     });
+
+    /* Percentages */
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        document.querySelectorAll(".skill").forEach((skill) => {
+          const percentage = parseInt(skill.getAttribute("data-percentage"));
+          const percentageText = skill.querySelector(".percentage-text");
+          const percentageFill = skill.querySelector(".percentage-fill");
+          let count = 0;
+          const counter = setInterval(() => {
+            count = Math.min(count + 1, percentage);
+            percentageText.textContent = `${count}%`;
+            percentageFill.style.width = `${count}%`;
+            if (count >= percentage) clearInterval(counter);
+          }, 25);
+        });
+        observer.disconnect();
+      }
+    },
+    { rootMargin: "0px 0px -200px 0px" }
+  );
+  observer.observe(document.querySelector(".skills"));
+});
